6e7ad334507557e3e1651b3a4955846021d72b12
hugosenari <hugosenari@gmail.com>
2022-03-17 18:25:16 -0300

    chore: re-index ../nimpkgs/e/eris_utils/0_1_3

--- 0_1_3/flake.nix
+++ 0_1_3/flake.nix
@@ -11,7 +11,7 @@
   inputs.src-eris_utils-0_1_3.owner = "~ehmry";
   inputs.src-eris_utils-0_1_3.ref   = "refs/tags/0.1.3";
   inputs.src-eris_utils-0_1_3.repo  = "eris_utils";
-  inputs.src-eris_utils-0_1_3.type  = "other";
+  inputs.src-eris_utils-0_1_3.type  = "sourcehut";
   
   inputs."eris".owner = "nim-nix-pkgs";
   inputs."eris".ref   = "master";
